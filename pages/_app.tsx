de4800efde02e7106fe30ec6ed0942444e849fc7
Octanium <36778771+Octanium0@users.noreply.github.com>
2020-12-08 10:53:23 +0100

    test1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import AuthProvider from '@context/auth';
 
 import '@src/styles/globals.css';
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />;
+    </AuthProvider>
+  );
 }
 
 export default App;
---
+++ src/context/auth.tsx
@@ -0,0 +1,66 @@
+import React, { createContext, useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+import { initializeFirebase } from '@services/firebase/client';
+
+initializeFirebase();
+export interface Auth {
+  uid: string;
+}
+
+const initialValue = {
+  uid: null,
+};
+
+export const authContext = createContext<Auth>(initialValue);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const auth = useAuthProvider();
+
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+};
+
+export const useAuthProvider = (): Auth => {
+  const [uid, setUid] = useState<string>(null);
+
+  useEffect(() => {
+    firebase
+      .auth()
+      .signInAnonymously()
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorMessage, errorCode);
+      });
+
+    const cancelAuthListener = firebase.auth().onIdTokenChanged((user) => {
+      if (user) {
+        setUid(user.uid);
+      } else {
+        setUid(null);
+      }
+    });
+
+    return () => {
+      cancelAuthListener();
+    };
+  }, []);
+
+  return {
+    uid,
+  };
+};
+
+export const withAuth = <P extends Record<string, any>>(
+  Component: React.ComponentType<P>
+): React.ComponentType<P> =>
+  class WithAuth extends React.Component<P> {
+    render() {
+      return (
+        <AuthProvider>
+          <Component {...this.props} />
+        </AuthProvider>
+      );
+    }
+  };
+
+export default AuthProvider;
